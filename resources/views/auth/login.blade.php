e64f54fe61aa033ce70dc863d8d3e00303d993c0
Mark Balajadia <markb.freelance@gmail.com>
2025-03-29 19:48:14 +0800

    login page UI update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,14 @@
     <!-- Email Address -->
     <div>
       <x-input-label for="email" :value="__('Email')" />
-      <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="Enter Email" />
+      <x-text-input
+        id="email"
+        class="block mt-1 w-full focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
+        type="email"
+        name="email"
+        :value="old('email')"
+        required autofocus autocomplete="username"
+        placeholder="Enter Email" />
       <x-input-error :messages="$errors->get('email')" class="mt-2" />
     </div>
 
@@ -16,10 +23,13 @@
     <div class="mt-4">
       <x-input-label for="password" :value="__('Password')" />
 
-      <x-text-input id="password" class="block mt-1 w-full"
+      <x-text-input
+        id="password"
+        class="block mt-1 w-full focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
         type="password"
         name="password"
-        required autocomplete="current-password" placeholder="Enter Password" />
+        required autofocus autocomplete="current-password"
+        placeholder="Enter Password" />
 
       <x-input-error :messages="$errors->get('password')" class="mt-2" />
     </div>
@@ -27,19 +37,19 @@
     <!-- Remember Me -->
     <div class="block mt-4">
       <label for="remember_me" class="inline-flex items-center">
-        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-green-700 shadow-sm focus:ring-green-700" name="remember">
         <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
       </label>
     </div>
 
     <div class="flex items-center justify-end mt-4">
       @if (Route::has('password.request'))
-      <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+      <a class="underline text-sm text-gray-600 hover:text-yellow-800 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
         {{ __('Forgot your password?') }}
       </a>
       @endif
 
-      <x-primary-button class="ms-3">
+      <x-primary-button class="ms-3 bg-green-700 hover:bg-green-900">
         {{ __('Log in') }}
       </x-primary-button>
     </div>
