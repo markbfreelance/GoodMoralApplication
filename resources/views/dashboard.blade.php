88ea13fec5e591efa924a569c35f7facc163c08f
Arjayy <johnarjay.camarao@gmail.com>
2025-04-18 01:11:33 +0800

    message@dashboard student

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,29 +43,35 @@
         </div>
         <!-- Apply for Good Moral Certificate Button -->
         {{-- Check if the user has any violations --}}
+        <!-- Violation Section -->
         <div class="p-6">
-          <h3 class="text-lg font-semibold mb-4 text-red-600">You have existing violation(s).</h3>
+          @if ($Violation->isEmpty())
+        <h3 class="text-lg font-semibold text-green-600">You have no existing violations. </h3>
+      @else
+      <h3 class="text-lg font-semibold mb-4 text-red-600">You have existing violation(s).</h3>
 
-          {{-- Show the violation(s) in a table --}}
-          <table class="min-w-full bg-white border border-gray-300 rounded-lg">
-            <thead>
-              <tr class="text-left border-b bg-gray-100">
-                <th class="py-2 px-4">Offense Type</th>
-                <th class="py-2 px-4">Description</th>
-                <th class="py-2 px-4">Date Committed</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($Violation as $violation)
-          <tr class="border-b">
-          <td class="py-2 px-4">{{ $violation->offense_type }}</td>
-          <td class="py-2 px-4">{{ $violation->violation}}</td>
-          <td class="py-2 px-4">{{ $violation->created_at->format('M d, Y') }}</td>
-          </tr>
-        @endforeach
-            </tbody>
-          </table>
+      {{-- Show the violation(s) in a table --}}
+      <table class="min-w-full bg-white border border-gray-300 rounded-lg">
+      <thead>
+        <tr class="text-left border-b bg-gray-100">
+        <th class="py-2 px-4">Offense Type</th>
+        <th class="py-2 px-4">Description</th>
+        <th class="py-2 px-4">Date Committed</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($Violation as $violation)
+      <tr class="border-b">
+      <td class="py-2 px-4">{{ $violation->offense_type }}</td>
+      <td class="py-2 px-4">{{ $violation->violation }}</td>
+      <td class="py-2 px-4">{{ $violation->created_at->format('M d, Y') }}</td>
+      </tr>
+    @endforeach
+      </tbody>
+      </table>
+    @endif
         </div>
+
 
 
 
