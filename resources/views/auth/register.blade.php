0f0e924fa18c8799e9bc8dd89082d4da51679bd1
Arjayy <johnarjay.camarao@gmail.com>
2025-04-20 10:19:58 +0800

    Directory for SignUp/SignIn

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,41 +1,55 @@
+<header class="w-full max-w-4xl mx-auto p-6 flex items-center justify-between">
+  <span class="text-green-800 font-semibold text-2xl">St. Paul University Philippines</span>
+  <nav class="space-x-2">
+    @if (Route::has('login'))
+    @auth
+    <a href="{{ url('/dashboard') }}"
+      class="inline-block px-5 py-2 text-gray-600 border border-green-700 hover:bg-yellow-400 hover:text-white rounded-md">
+      Go to Dashboard
+    </a>
+  @else
+  <a href="{{ route('login') }}"
+    class="inline-block px-5 py-2 text-gray-600 border border-green-700 hover:bg-yellow-400 hover:text-white rounded-md">
+    Sign In
+  </a>
+  @if (Route::has('register'))
+    <a href="{{ route('register') }}"
+    class="inline-block px-5 py-2 text-gray-600 border border-green-700 hover:bg-yellow-400 hover:text-white rounded-md">
+    Create an Account
+    </a>
+  @endif
+@endauth
+  @endif
+  </nav>
+</header>
 <x-guest-layout>
+
   <form method="POST" action="{{ route('register') }}">
     @csrf
 
     <!-- Name -->
     <div>
       <x-input-label for="fname" :value="__('First Name')" />
-      <x-text-input
-        id="fname"
+      <x-text-input id="fname"
         class="block mt-1 w-full uppercase focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        type="text"
-        name="fname"
-        :value="old('fname')"
-        required autofocus autocomplete="fname"
+        type="text" name="fname" :value="old('fname')" required autofocus autocomplete="fname"
         placeholder="Enter First Name" />
       <x-input-error :messages="$errors->get('fname')" class="mt-2" />
     </div>
     <!-- -->
     <div class="mt-4">
       <x-input-label for="lname" :value="__('Last name')" />
-      <x-text-input
-        id="lname"
+      <x-text-input id="lname"
         class="block mt-1 w-full uppercase focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        type="text" name="lname"
-        :value="old('lname')"
-        required autocomplete="lname"
-        placeholder="Enter Last Name" />
+        type="text" name="lname" :value="old('lname')" required autocomplete="lname" placeholder="Enter Last Name" />
       <x-input-error :messages="$errors->get('lname')" class="mt-2" />
     </div>
 
     <div class="mt-4">
       <x-input-label for="department" :value="__('Department')" />
-      <select
-        id="department"
-        name="department"
-        required
+      <select id="department" name="department" required
         class="block mt-1 w-full text-gray-500 border-gray-300 rounded-md shadow-sm focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100">
-        <option value=""  disabled selected>Select Department</option>
+        <option value="" disabled selected>Select Department</option>
         <option value="SITE">SITE</option>
         <option value="SBAHM">SBAHM</option>
         <option value="SASTE">SASTE</option>
@@ -49,13 +63,9 @@
     <div class="mt-4">
       <!-- Alpine.js for dynamic input toggling -->
       <x-input-label for="account_type" :value="__('Account Type')" />
-      <select
-        id="account_type"
-        name="account_type"
+      <select id="account_type" name="account_type"
         class="block mt-1 w-full text-gray-500 border-gray-300 rounded-md shadow-sm focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        required
-        x-data
-        x-on:change="$dispatch('account-type-changed', $event.target.value)">
+        required x-data x-on:change="$dispatch('account-type-changed', $event.target.value)">
         <option value="" disabled selected>Select Account Type</option>
         <option value="student">Student</option>
         <option value="alumni">Alumni</option>
@@ -67,13 +77,9 @@
     <!-- Student ID  -->
     <div class="mt-4">
       <x-input-label for="student_id" :value="__('Student ID')" />
-      <x-text-input
-        id="student_id"
+      <x-text-input id="student_id"
         class="block mt-1 w-full focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        type="text"
-        name="student_id"
-        :value="old('student_id')"
-        required autocomplete="student_id"
+        type="text" name="student_id" :value="old('student_id')" required autocomplete="student_id"
         placeholder="Enter Student ID" />
       <x-input-error :messages="$errors->get('student_id')" class="mt-2" />
     </div>
@@ -81,26 +87,18 @@
     <!-- Year level  -->
     <div class="mt-4">
       <x-input-label for="year_level" :value="__('Year Level')" />
-      <x-text-input
-        id="year_level"
+      <x-text-input id="year_level"
         class="block mt-1 w-full focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        type="text"
-        name="year_level"
-        :value="old('year_level')"
-        required placeholder="Year / Course" />
+        type="text" name="year_level" :value="old('year_level')" required placeholder="Year / Course" />
       <x-input-error :messages="$errors->get('year_level')" class="mt-2" />
     </div>
 
     <!-- Email Address -->
     <div class="mt-4">
       <x-input-label for="email" :value="__('Email')" />
-      <x-text-input
-        id="email"
+      <x-text-input id="email"
         class="block mt-1 w-full focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        type="email"
-        name="email"
-        :value="old('email')"
-        required autocomplete="username"
+        type="email" name="email" :value="old('email')" required autocomplete="username"
         placeholder="Entel Valid Email" />
       <x-input-error :messages="$errors->get('email')" class="mt-2" />
     </div>
@@ -109,13 +107,9 @@
     <div class="mt-4">
       <x-input-label for="password" :value="__('Password')" />
 
-      <x-text-input
-        id="password"
+      <x-text-input id="password"
         class="block mt-1 w-full focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        type="password"
-        name="password"
-        required autocomplete="new-password"
-        placeholder="Enter Password" />
+        type="password" name="password" required autocomplete="new-password" placeholder="Enter Password" />
 
       <x-input-error :messages="$errors->get('password')" class="mt-2" />
     </div>
@@ -124,19 +118,17 @@
     <div class="mt-4">
       <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-      <x-text-input
-        id="password_confirmation"
+      <x-text-input id="password_confirmation"
         class="block mt-1 w-full focus:border-green-700 focus:ring-1 focus:ring-green-700 focus:ring-opacity-100"
-        type="password"
-        name="password_confirmation"
-        required autocomplete="new-password"
+        type="password" name="password_confirmation" required autocomplete="new-password"
         placeholder="Confirm Password" />
 
       <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
     </div>
 
     <div class="flex items-center justify-end mt-4">
-      <a class="underline text-sm text-gray-600 hover:text-yellow-800 focus:outline-none focus:ring-0 focus:ring-offset-0" href="{{ route('login') }}">
+      <a class="underline text-sm text-gray-600 hover:text-yellow-800 focus:outline-none focus:ring-0 focus:ring-offset-0"
+        href="{{ route('login') }}">
         {{ __('Already registered?') }}
       </a>
 
