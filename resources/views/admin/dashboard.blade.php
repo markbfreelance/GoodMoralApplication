0dd3d3d4e0cf316c418a2cc9bf4251240d8934da
Mark Balajadia <markb.freelance@gmail.com>
2025-04-23 03:09:04 +0800

    admin dashboard sidebar icons

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -59,7 +59,9 @@
 
         <!-- PSG application -->
         <a href="{{ route('admin.psgApplication') }}" class="gap-2 h-20 items-center flex px-4 py-2 text-gray-700 hover:bg-gray-700 hover:text-gray-300">
-          <x-icon-logout class="h-10 w-10 text-gray-700 hover:text-red-600" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+          </svg>
           <span class="text-white">PSG Application</span>
         </a>
 
@@ -67,12 +69,12 @@
         <form method="POST" action="{{ route('logout') }}">
           @csrf
           <a href="{{ route('logout') }}"
-            class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:text-red-600"
+            class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-700 hover:text-red-600"
             onclick="event.preventDefault(); this.closest('form').submit();">
             <x-icon-logout class="w-10 h-10" />
             {{ __('') }}
+            <span class="text-white hover:text-red-600">LOGOUT</span>
           </a>
-          <span class="text-white">LOGOUT</span>
         </form>
 
       </nav>
