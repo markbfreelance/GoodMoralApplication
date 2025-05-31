4521e230b89efdaf675e7e179677ceaf3ddbe60d
Arjaayy <johnarjay.camarao@gmail.com>
2025-06-01 00:07:13 +0800

    minor fix

--- resources/views/sec_osa/sidebar.blade.php
+++ resources/views/sec_osa/sidebar.blade.php
@@ -12,16 +12,17 @@
     </a>
 
     <!-- Applications -->
-    <a href="{{ route('sec_osa.application') }}" class="gap-2 h-20 items-center flex px-4 py-2 text-gray-700 hover:bg-gray-700 hover:text-gray-300">
+    <a href="{{ route('sec_osa.application') }}"
+      class="gap-2 h-20 items-center flex px-4 py-2 {{ request()->routeIs('sec_osa.application') ? 'bg-gray-700 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-gray-300' }}">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
       </svg>
       <span class="text-white whitespace-nowrap">Applications</span>
     </a>
 
-        <!-- Minor Violations -->
+    <!-- Minor Violations -->
     <a href="{{ route('sec_osa.minor') }}"
-      class="gap-2 h-20 items-center flex px-4 py-2 text-gray-700 hover:bg-gray-700 hover:text-gray-300">
+      class="gap-2 h-20 items-center flex px-4 py-2 {{ request()->routeIs('sec_osa.minor') ? 'bg-gray-700 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-gray-300' }}">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
         stroke="currentColor" class="size-10">
         <path stroke-linecap="round" stroke-linejoin="round"
@@ -32,7 +33,7 @@
 
     <!-- Major Violations -->
     <a href="{{ route('sec_osa.major') }}"
-      class="gap-2 h-20 items-center flex px-4 py-2 text-gray-700 hover:bg-gray-700 hover:text-gray-300">
+      class="gap-2 h-20 items-center flex px-4 py-2 {{ request()->routeIs('sec_osa.major') ? 'bg-gray-700 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-gray-300' }}">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
         stroke="currentColor" class="size-10">
         <path stroke-linecap="round" stroke-linejoin="round"
